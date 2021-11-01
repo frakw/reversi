6fcaaf4a9dd67398fea22fc0958f10c29b3acdec
frakw <frakwu@gmail.com>
2021-11-01 21:44:09 +0800

    version 0.877

--- reversi/reversi/main.cpp
+++ reversi/reversi/main.cpp
@@ -2,9 +2,9 @@
 // If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
 // (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan graphics context creation, etc.)
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include <stdio.h>
 #include <iostream>
 #include <string>
